419f0cb018dc3242966e06ff22d19eb6de012e87
Dilip <dilip_agheda@yahoo.com>
2019-06-21 20:15:39 +1000

    Blog loop is now done properly using standard wordpress method

--- partials/blog-loop.php
+++ partials/blog-loop.php
@@ -1,55 +1,30 @@
-<?php 
 
-
-
-if(have_posts()) : 
-	$args = array(
-		'numberposts' => 13,
-		'offset' => 0,
-		'orderby' => 'post_date',
-		'order' => 'DESC',
-		'post_type' => 'post',
-		'post_status' => 'publish'	
-	);
+<?php if(have_posts()) : ?>
 	
-	$recent_posts = wp_get_recent_posts($args);
-?>
 <div class="container">
-	<div class="row">
-		<?php for($x = 0; $x < count($recent_posts); $x++) { 
-			
-			// echo '<pre>';
-			// print_r($recent_posts[$x]);
-			// echo '</pre>';
-			set_query_var( 'current_post', $recent_posts[$x] );
-			if($x==0) : 
+	<div class="row mb-5">
+		<?php $count=0; while(have_posts()) : the_post(); 
+			if($count==0) : 
 				get_template_part('partials/hero-one');
 			else :  
-				if($x==1) : 
+				if($count==1) : 
 					get_template_part('partials/hero-two');
 				else:
-					if($x==2):
+					if($count==2):
 						get_template_part('partials/hero-three');
 					else:
-						if($x==9):
+						if($count==9):
 							get_template_part('partials/hero-one');
 						else:
 							get_template_part('partials/blog-normal');
 						endif;
 					endif;
-				endif;		
-			?>
+				endif;	
+			endif;	
+			$count++;
+			endwhile; ?>
 				
-			<?php endif; } ?>
 
 	</div>
-<?php else :  ?>
-	<?php if(is_search()) : ?>
-	<div class="container">
-		<div class="row">
-			<div class="span12"><p class="no-results"><?php echo __('No results were found. Please try a different search.', 'semplice'); ?></p></div>
-		</div>
-	</div>
-	<?php endif; ?>
-<?php endif; ?>
 </div>
+<?php endif; ?>
