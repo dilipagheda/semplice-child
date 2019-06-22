3969c8ae4e87c8478a8eb72306991f54c8dce0be
Dilip <dilip_agheda@yahoo.com>
2019-06-22 13:49:22 +1000

    Added AJAX function in script.js for load more

--- script.js
+++ script.js
@@ -31,4 +31,35 @@ $(function () {
 
     });
 
+    /* Ajax functions */
+    $(document).on('click','.eq-load-more', function(){
+      
+      var that = $(this);
+      var page = $(this).data('page');
+      var newPage = page+1;
+      var ajaxurl = that.data('url');
+      
+      $.ajax({
+        
+        url : ajaxurl,
+        type : 'post',
+        data : {
+          
+          page : page,
+          action: 'eq_load_more'
+          
+        },
+        error : function( response ){
+          console.log(response);
+        },
+        success : function( response ){
+          
+          that.data('page', newPage);
+          $('.eq-posts-container').append( response );
+          
+        }
+        
+      });
+      
+    });
   });
