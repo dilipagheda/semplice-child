b3e8c35357f30afb5a10355ac902b7ef454a843d
Dilip <dilip_agheda@yahoo.com>
2019-06-22 17:31:54 +1000

    bug fix- responsiveness of blog header

--- partials/blog-pagination.php
+++ partials/blog-pagination.php
@@ -1,4 +1,4 @@
-<div class="container text-center eq-load-more-container d-flex flex-column">
+<div class="container text-center eq-load-more-container">
 		<a class="btn btn-lg btn-default eq-load-more" data-page="1" data-url="<?php echo admin_url('admin-ajax.php'); ?>">
 			Load More Posts
 		</a>
