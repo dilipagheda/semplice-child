7cde9c9794ec4bc2cc7c8e355ade6d11c937b73c
Dilip <dilip_agheda@yahoo.com>
2019-06-22 18:36:08 +1000

    fixed intendation

--- partials/hero-one.php
+++ partials/hero-one.php
@@ -7,45 +7,43 @@ if(!$image){
 	$imageURL = $image[0];
 }
 ?>
-
 <div class="col-12 mt-3">
-					<article class="blog-hero-one">
-					<div class="card" style="height:450px">
-						<div class="blog-thumbnail" style="background-image:url(<?php echo $imageURL;?>)">
-							<div class="blog-overlay"></div>
-							<div class="content-container">
-								<span class="hero-one-category">
-									<?php echo the_category('','',$id) ?>
+	<article class="blog-hero-one">
+		<div class="card" style="height:450px">
+			<div class="blog-thumbnail" style="background-image:url(<?php echo $imageURL;?>)">
+				<div class="blog-overlay"></div>
+				<div class="content-container">
+					<span class="hero-one-category">
+						<?php echo the_category('','',$id) ?>
+					</span>
+					<h1 class="h1-size-big">
+						<a href=<?php echo get_permalink($id)?>>
+							<?php echo get_the_title($id) ?>
+						</a>
+					</h1>
+					<div class="divider margin-bottom-100"></div>
+					<div class="content-footer">
+							<span>
+								<?php echo get_the_author($id) ?>
+							</span>
+							<div class="content-subfooter">
+								<span class="date">
+									<?php echo get_the_date('j F Y',$id) ?>
 								</span>
-								<h1 class="h1-size-big">
-									<a href=<?php echo get_permalink($id)?>>
-										<?php echo get_the_title($id) ?>
-									</a>
-								</h1>
-								<div class="divider margin-bottom-100"></div>
-								<div class="content-footer">
-										<span>
-											<?php echo get_the_author($id) ?>
-										</span>
-										<div class="content-subfooter">
-											<span class="date">
-												<?php echo get_the_date('j F Y',$id) ?>
-											</span>
-											
-											<div class="reading-time">
-												<img src="<?php echo get_stylesheet_directory_uri() . '/images/clock-white.svg' ?>" > 
-												<span>
-													<?php 
-													$content = get_post_field('post_content', $id);
-													echo ceil(str_word_count(trim( strip_tags($content)))/200);
-													?> min read
-												</span>									
-											</div>
-										</div>
+								
+								<div class="reading-time">
+									<img src="<?php echo get_stylesheet_directory_uri() . '/images/clock-white.svg' ?>" > 
+									<span>
+										<?php 
+										$content = get_post_field('post_content', $id);
+										echo ceil(str_word_count(trim( strip_tags($content)))/200);
+										?> min read
+									</span>									
 								</div>
 							</div>
-							
-						</div>
 					</div>
-					</article>
+				</div>
+			</div>
+		</div>
+	</article>
 </div>
