2dd4bb1bac3fea9e0e7e3398660f3c04206e0f40
Dilip <dilip_agheda@yahoo.com>
2019-06-23 10:16:08 +1000

    Added a back icon and link

--- partials/blog-header.php
+++ partials/blog-header.php
@@ -10,11 +10,22 @@ if ( !$title ) {
 		<div class="container m-auto">
 			<div class="row">
 				<div class="col-12 col-md-9 m-auto blog-header">
-					<a style="text-decoration:none" href="<?php echo get_permalink( get_option( 'page_for_posts' ) ); ?>">
-						<h1 class="display-4">
+					<?php if($title=='Blog'): ?>
+						<a style="text-decoration:none" href="<?php echo get_permalink( get_option( 'page_for_posts' ) ); ?>">
+							<h1 class="display-4">
+								<?php echo $title ?>
+							</h1>
+						</a>
+					<?php else: ?>
+						<h1 class="display-3">
 							<?php echo $title ?>
 						</h1>
-					</a>
+						<a style="text-decoration:none" href="<?php echo get_permalink( get_option( 'page_for_posts' ) ); ?>">
+							<div class="pt-2 pb-2">
+								<img style="height:30px;width:30px" src="<?php echo get_stylesheet_directory_uri() . '/images/back.png' ?>">Back
+							</div>
+						</a>
+					<?php endif; ?>
 				</div>
 				<div class="col-12 col-md-3 m-auto d-flex subscribe-button-container">
 					<!-- Button trigger modal -->
